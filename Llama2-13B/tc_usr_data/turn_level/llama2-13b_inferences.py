cb14800342d9f655c7c8bff0a372bafd5c5aa371
raffaele-aurucci <raffaeleaurucci.informatica@gmail.com>
2025-01-23 17:57:57 +0100

    added other tests
    Co-authored-by: Angelo Palmieri <palmieri309@gmail.com>
    Co-authored-by: Sabino Malafronte <sabinomalafronte27@gmail.com>

--- Llama2-13B/tc_usr_data/turn_level/llama2-13b_inferences.py
+++ Llama2-13B/tc_usr_data/turn_level/llama2-13b_inferences.py
@@ -58,7 +58,7 @@ def make_inferences():
 
     # Load TC_USR dataset.
     with open('../../../_datasets/tc_usr_data.json', 'r') as file:
-        fed_data = json.load(file)
+        tc_usr_data = json.load(file)
 
     # File to save dialogue ratings.
     file_path = 'llama2-13b_dialogue_ratings.json'
@@ -78,65 +78,65 @@ def make_inferences():
         formatted_dialogues = []
 
 
-    # Iterate over dataset FED.
-    for dialog_id, example in enumerate(tqdm(fed_data, desc="Dialogue ratings progress")):
-        context = example["context"]
-        response = example.get("response")
-        context = context.split("\n")
-        context = [s.replace("User: ", "").replace("System: ", "").strip() for s in context]
-        if response is None:
-            # this is a conversation data point, not a turn data point
-            continue
+    # Iterate over dataset tc_usr_data.
+    for dialog_id, example in enumerate(tqdm(tc_usr_data, desc="Dialogue ratings progress")):
+        context = example["context"].split("\n")
+        responses = example["responses"]
 
-        response = response.replace("User: ", "").replace("System: ", "").strip()
-        # full_conversation = " ".join(conversation) + " " + response
+        for response_data in responses:
+            response = response_data["response"].split("\n")[0]
 
-        prompt = create_prompt(context, response)
+            if response is None:
+                # this is a conversation data point, not a turn data point
+                continue
 
-        input_tokens = tokenizer.encode(prompt, add_special_tokens=False, return_tensors="pt")
-        output_token_yes = tokenizer.encode("Yes", add_special_tokens=False, return_tensors="pt")[0]
-        output_token_no = tokenizer.encode("No", add_special_tokens=False, return_tensors="pt")[0]
+            response = [response]
+            prompt = create_prompt(context, response)
 
-        output_tokens = [output_token_yes, output_token_no]
+            input_tokens = tokenizer.encode(prompt, add_special_tokens=False, return_tensors="pt")
+            output_token_yes = tokenizer.encode("Yes", add_special_tokens=False, return_tensors="pt")[0]
+            output_token_no = tokenizer.encode("No", add_special_tokens=False, return_tensors="pt")[0]
 
-        output_tokens_prob = []
+            output_tokens = [output_token_yes, output_token_no]
 
-        # Predict with the given model
-        with torch.no_grad():
-            outputs = model.generate(input_tokens, max_new_tokens=1, output_logits=True, return_dict_in_generate=True)
-            logit_predictions = outputs.logits[0]
+            output_tokens_prob = []
 
-        for output_token in output_tokens:
-            # Decode output token
-            token = tokenizer.decode(output_token)
+            # Predict with the given model
+            with torch.no_grad():
+                outputs = model.generate(input_tokens, max_new_tokens=1, output_logits=True, return_dict_in_generate=True)
+                logit_predictions = outputs.logits[0]
 
-            # logit_predictions is a tensor of dimensions [1, vocab_size], where each value represents an unnormalized logit for a token in the vocabulary.
-            log_probs = torch.nn.functional.log_softmax(logit_predictions, dim=-1)
+            for output_token in output_tokens:
+                # Decode output token
+                token = tokenizer.decode(output_token)
 
-            # Extract the log probability of the output token
-            out_token_log_prob = log_probs[0, output_token]
+                # logit_predictions is a tensor of dimensions [1, vocab_size], where each value represents an unnormalized logit for a token in the vocabulary.
+                log_probs = torch.nn.functional.log_softmax(logit_predictions, dim=-1)
 
-            # Extract the probability of the output token
-            out_token_prob = math.exp(out_token_log_prob)
+                # Extract the log probability of the output token
+                out_token_log_prob = log_probs[0, output_token]
 
-            output_tokens_prob.append({token: out_token_prob})
+                # Extract the probability of the output token
+                out_token_prob = math.exp(out_token_log_prob)
 
-            # print("============")
-            # print(f"Token: {token}", "log prob: ", out_token_log_prob, 'prob: ', out_token_prob)
-            # print("============")
+                output_tokens_prob.append({token: out_token_prob})
+
+                # print("============")
+                # print(f"Token: {token}", "log prob: ", out_token_log_prob, 'prob: ', out_token_prob)
+                # print("============")
 
 
-        # Normalized probability as in the paper ("Yes" is our score to considering)
-        output_tokens_prob[0]['Yes'] = output_tokens_prob[0]['Yes'] / (output_tokens_prob[0]['Yes'] + output_tokens_prob[1]['No'])
-        output_tokens_prob[1]['No'] = 1 - output_tokens_prob[0]['Yes']
+            # Normalized probability as in the paper ("Yes" is our score to considering)
+            output_tokens_prob[0]['Yes'] = output_tokens_prob[0]['Yes'] / (output_tokens_prob[0]['Yes'] + output_tokens_prob[1]['No'])
+            output_tokens_prob[1]['No'] = 1 - output_tokens_prob[0]['Yes']
 
-        formatted_data = process_list(dialog_id, output_tokens_prob)
-        formatted_dialogues.append(formatted_data)
+            formatted_data = process_list(dialog_id, output_tokens_prob)
+            formatted_dialogues.append(formatted_data)
 
-        with open(file_path, 'w') as json_file:
-            json.dump({"dialogues": formatted_dialogues}, json_file, indent=4)
+            with open(file_path, 'w') as json_file:
+                json.dump({"dialogues": formatted_dialogues}, json_file, indent=4)
 
-        # print(f"Dialogue {i} ratings write successfully!")
+            # print(f"Dialogue {i} ratings write successfully!")
 
 
 if __name__ == '__main__':
